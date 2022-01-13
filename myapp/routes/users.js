eb6b6d41a248d69e5c9415a820dd81cff20f410e
wooseokhan <ws.han@twolinecode.com>
2022-01-13 15:23:16 +0900

    expressJS CRUD

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -6,42 +6,57 @@ var app = express();
 app.use(cors());
 
 var connection = maraidb.createConnection({
-  host: 'nowrica.synology.me',
-  port: 33336,
-  user: 'root',
-  password: '1234',
-  database: 'express'
+    host: 'nowrica.synology.me',
+    port: 33336,
+    user: 'root',
+    password: '1234',
+    database: 'express'
 })
 connection.connect();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  connection.query(`select * from users`, (err, rows, fields) => {
-    res.json(rows);
-  })
-});
-//
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
-
-router.post('/', function(req, res, next) {
-  let name = req.body.name;
-  let age = req.body.age;
-  console.log(typeof name);
-  console.log(typeof age);
-  connection.query(`insert into users(name,age) values('${name}', ${age})`, (err, rows, field) => {
-    console.log(err);
-    res.json('성공');
-  })
+router.get('/', function (req, res, next) {
+    if (!req.query.name) {
+        connection.query(`select * from users`, (err, rows, fields) => {
+            res.json(rows);
+        })
+    }
+    next();
 });
 
-// router.put('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
-//
-// router.delete('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
+router.get('/', function (req, res, next) {
+    if (req.query.name) {
+        let name = req.query.name;
+        connection.query(`select * from users where name = '${name}'`, (err, rows, fields) => {
+            res.json(rows);
+        })
+    }
+});
+
+router.post('/', function (req, res, next) {
+    let name = req.body.name;
+    let age = req.body.age;
+    connection.query(`insert into users(name,age) values('${name}', ${age})`, (err, rows, fields) => {
+        console.log(err);
+        res.json('성공');
+    })
+});
+
+router.put('/', function (req, res, next) {
+    let name = req.body.name;
+    let age = req.body.age;
+    connection.query(`UPDATE users SET age = ${age} where name = '${name}'`, (err, rows, fields) => {
+        console.log(err, rows, fields)
+        res.json(`${name}의 나이 변경 성공`);
+    })
+});
+
+router.delete('/', function (req, res, next) {
+    let name = req.body.name;
+    connection.query(`DELETE FROM users WHERE name = '${name}'`, (err, rows, fields) => {
+        console.log((err, rows, fields));
+        res.json(`${name} 삭제 성공`);
+    })
+});
 
 module.exports = router;
